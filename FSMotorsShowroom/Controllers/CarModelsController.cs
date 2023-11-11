4202f7b88879cfe8122c23518034dc500656c83a
IdreesDeveloper <144520269+IdreesDeveloper@users.noreply.github.com>
2023-11-11 16:47:20 +0500

    links updated

--- FSMotorsShowroom/Controllers/CarModelsController.cs
+++ FSMotorsShowroom/Controllers/CarModelsController.cs
@@ -50,9 +50,6 @@ namespace FSMotorsShowroom.Controllers
             return View();
         }
 
-        // POST: CarModels/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("CarModelId,CarModelName")] CarModel carModel)
